3cb7f506c938ad069c92d5b16d7d1f6b052e26aa
vraj marvania <vraj.marvania@gmail.com>
2021-03-21 12:53:02 +0530

    ook

--- Covid19/Covid19/urls.py
+++ Covid19/Covid19/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
+from Covid19.Covid19 import settings
+from Covid19.Covid19app.views import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Covid19app.urls'))
+    path('',include('Covid19app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]

800d4affa416e62f8daef4b85fb88f8b4b3067bc
vraj marvania <vraj.marvania@gmail.com>
2021-03-21 15:12:36 +0530

    ook

--- Covid19/Covid19/urls.py
+++ Covid19/Covid19/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 
-from Covid19.Covid19 import settings
-from Covid19.Covid19app.views import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
